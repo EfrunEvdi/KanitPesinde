e2ed049100e2ca884b267bae1e3f95b9c986c365
EfrunEvdi <efruncetkin@gmail.com>
2022-12-26 16:13:45 +0300

    Accused 14- 2 hazı9r.

---
+++ KanitPesindeProject/Controllers/AccusedController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_KanitPesindeProject.Controllers
+{
+    public class AccusedController : Controller
+    {
+        public IActionResult Accused1()
+        {
+            return View();
+        }
+        public IActionResult Accused2()
+        {
+            return View();
+        }
+    }
+}
